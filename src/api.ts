ff798836bd7f9217adc1479aba584926f8e55c79
Lisergicus <pizio54@hotmail.it>
2021-11-02 20:06:05 +0100

    0.2

--- src/api.ts
+++ src/api.ts
@@ -14,7 +14,7 @@ interface BlogPost {
   draft: boolean;
 }
 
-const posts: BlogPost[] = [
+let posts: BlogPost[] = [
   {
     id: 0,
     title: "first post",
@@ -38,7 +38,34 @@ app.get("/posts/", (req, res) => {
 app.get("/posts/:id", (req, res) => {
   const id = Number(req.params.id);
   const post = posts.find((post) => post.id == id);
+  if (!post) {
+    return res.status(404).send({ msg: "not found" });
+  }
   res.send(post);
+});
+
+app.delete("/posts/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const postToDelete = posts.find((post) => post.id == id);
+  if (!postToDelete) {
+    return res.status(404).send({ msg: "not found" });
+  }
+  posts = posts.filter((post) => post.id != id);
+  return res.send(postToDelete);
+});
+
+app.post("/posts/", (req, res) => {
+  const postData = req.body;
+  const lastPost = posts[posts.length - 1];
+  const newPost: BlogPost = {
+    id: lastPost ? lastPost.id + 1 : 0,
+    date: new Date(),
+    draft: false,
+    title: postData.title,
+    body: postData.body,
+  };
+  posts.push(newPost);
+  return res.status(201).send(newPost);
 });
 
 const PORT = 3000;
